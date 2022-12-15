d33cdc31379f7012f2f3205e14e449e4a3b3353a
PronCan <twojini98@gmail.com>
2022-12-15 09:53:18 +0900

    221214

--- set_time/index.js
+++ set_time/index.js
@@ -1,32 +1,27 @@
 const axios = require("axios");
 // import axios from 'axios';
-//import json from './key.json';
-//const json = require("./key.json");
-const key = "https://discord.com/api/webhooks/1052026751368319027/ckxz1IW8XrN2Itskvzf0EFBjg41dsgz6RrMUORXZd0m5HJz5JswDPI3nMKlENaueqg2V";
-const key_test = "https://discord.com/api/webhooks/1052092847794421760/TE31bONmH84QvkpnlRpb3yelW2j8yjnd8idyi_meAj4zRSUsyuUR-vLM43lCbf8i1L9B";
-const yongkimchi = "https://discord.com/api/webhooks/1052100808969310268/spQECP2iG5Ampf47RTAsFw-4jeKqXjD0bGQZ2-rvKT8XZg9kMvMuVXhMdthkhMT_P_6O";
+const s_json = require("./key.json");
+const s_data_string = require("./data.json");
 
-//const key = json.key;
+const setUTCtoKor = 9*60*60*1000;
+const babo = s_json.babo;
+//const bosam_def = json.bosam_def;
+// const bosam_poke = s_json.bosam_poke;
 
 exports.handler = async(event) => {
     try {
-        let date = new Date();
-        let month = date.getMonth()+1;
-        let day = date.getDate();
-        
-        let hour = date.getHours();
-        let min = date.getMinutes();
-        let sec = date.getSeconds();
+        let date = new Date().toLocaleString();
+        //example endDate
+        //let dateData = new Date(data.data[0]);
+        let resText = s_json;
         // discord url, content
-        switch(time) {
-            // case 
-        }
-        const result = await axios.post(key, {
-            "content": "test message"
+        // const result = await axios.post(bosam_poke, {
+        const result = await axios.post(babo, {
+            "content": date
         });
         console.log("success", result);
     }catch(e) {
-        console.err("faild", e);
+        console.log("faild", e);
     }
 
     // TODO implement
