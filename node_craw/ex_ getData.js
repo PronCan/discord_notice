35bb7c1d453a40419120b68cde1da8e2dea2c20f
PronCan <twojini98@gmail.com>
2022-12-27 14:52:14 +0900

    ex_data

---
+++ node_craw/ex_ getData.js
@@ -0,0 +1,31 @@
+
+    {
+        "@context": "http://schema.org",
+        "@type": "Article",
+        "headline": "ポケモンSV攻略｜スカーレットバイオレット｜ゲームエイト",
+        "author":{
+          "@type": "Person",
+          "name": "ポケモンSV攻略班"
+        },
+        "publisher":{
+          "@type": "Organization",
+          "name": "Game8",
+          "url": "https://game8.jp/",
+          "logo":{
+              "@type": "ImageObject",
+              "url": "https://assets.game8.jp/public/logo.png"
+          }
+        },
+        "datePublished": "2022-06-01 17:23:20 +0900",
+        "dateModified": "2022-12-19 11:05:40 +0900",
+        "mainEntityOfPage":{
+          "@type": "WebPage",
+          "@id": "https://game8.jp/pokemon-sv"
+        },
+        "name": "ポケモンSV攻略｜スカーレットバイオレット｜ゲームエイト",
+        "url": "https://game8.jp/pokemon-sv",
+        "image": "https://assets.game8.jp/assets/game8_ogp-846ace0d45c153c1174a8beac78a5378ddc11fe3e20f7e4d9b51559cfc4db94d.png",
+        "thumbnailUrl": "https://assets.game8.jp/assets/game8_ogp-846ace0d45c153c1174a8beac78a5378ddc11fe3e20f7e4d9b51559cfc4db94d.png",
+        "description": "ポケモンSV(スカーレットバイオレット)の攻略サイトです。新ポケモンやストーリー攻略、ポケモン図鑑やレイドバトルなどの情報を最速でお届けします。ポケモンSVの攻略はGame8にお任せください！"
+      }
+    
